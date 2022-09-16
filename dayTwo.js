31e9971c4c1a10ea658cf149fcf85ba90d0ee7ec
ÁC Anyasodor <austinsodor@gmail.com>
2022-09-16 16:39:34 +0100

    first commit

---
+++ dayTwo.js
@@ -0,0 +1,23 @@
+let challenge = '30 days of javascript'
+console.log('challenge')
+console.log(challenge.length)
+
+//exercise level 2
+console.log('The quote \'There is no.... \' by john Holmes....')
+console.log(parseInt('10'))
+console.log(Math.round(9.8))
+
+let word = 'python' 
+console.log(word.includes('on'))
+
+let sentence = 'i hope this course is not full of jargons'
+console.log(sentence.includes('jargons'))
+
+console.log(Math.floor(Math.random() * 101))
+
+let course = 'javascript'
+console.log(course[Math.floor(Math.random() * course.length)])
+console.log('1\t 1\t 1\t \n 2\t 1\t 4 \n 1\t 5\t 25\t 125\t')
+
+let longSent = 'you cannot begin a sentence with because because because is a conjunction'
+console.log(longSent.slice(33,57))
